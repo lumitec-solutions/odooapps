0d4f9f3db92fe060e1af5c6e59e405986d7a2bb4
Cybrosys <cybroodoo@gmail.com>
2022-11-23 14:12:23 +0530

    [FIX] Fixed test errors

--- lt_double_opt_in/models/crm_lead.py
+++ lt_double_opt_in/models/crm_lead.py
@@ -32,21 +32,19 @@ class Lead(models.Model):
     def write(self, vals):
         """merge the leads with mailing contacts based on changes"""
         res = super(Lead, self).write(vals)
-        for record in self:
-            if record.email_from:
+        if 'country_id' in vals or 'tag_ids' in vals:
+            for record in self:
                 mailing_contacts = self.env['mailing.contact'].sudo().search(
                     [('email', '=', record.email_from)])
                 for mailing_contact in mailing_contacts:
-                    if 'country_id' in vals:
-                        mailing_contact.country_id = record.country_id
-                    if 'tag_ids' in vals:
-                        for tag in record.tag_ids:
-                            tag_id = self.env['mailing.tag'].search(
-                                [('name', '=', tag.name)], limit=1)
-                            if tag_id:
-                                mailing_contact.category_ids = [(4, tag_id.id)]
-                            else:
-                                add_tag = self.env['mailing.tag'].create({
-                                    'name': tag.name})
-                                mailing_contact.category_ids = [(4, add_tag.id)]
+                    mailing_contact.country_id = record.country_id
+                    for tag in record.tag_ids:
+                        tag_id = self.env['mailing.tag'].search(
+                            [('name', '=', tag.name)], limit=1)
+                        if tag_id:
+                            mailing_contact.category_ids = [(4, tag_id.id)]
+                        else:
+                            add_tag = self.env['mailing.tag'].create({
+                                'name': tag.name})
+                            mailing_contact.category_ids = [(4, add_tag.id)]
         return res
