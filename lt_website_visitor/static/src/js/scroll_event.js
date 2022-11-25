4265a0652e87b6d1676535b2d21d2f501c1a3078
Cybrosys <cybroodoo@gmail.com>
2022-11-25 12:53:46 +0530

    Added console

--- lt_website_visitor/static/src/js/scroll_event.js
+++ lt_website_visitor/static/src/js/scroll_event.js
@@ -13,6 +13,7 @@ odoo.define('lt_website_visitor.scroll_page', function (require) {
             });
             this._onScroll = function (ev) {
                 timer++;
+                console.log("timer--------", timer)
                 if(timer == 35){
                     self._rpc({
                         route: '/website/update_visitor_last_connection',
