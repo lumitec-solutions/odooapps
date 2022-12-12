4f7d8f158aac496b4637c5677c355759939a50fb
CybroOdooDev <cybroodoo@gmail.com>
2022-12-12 10:53:14 +0530

    [previous]

--- lt_double_opt_in/controllers/__init__.py
+++
@@ -1,7 +0,0 @@
-##############################################################################
-# Copyright (c) 2022 lumitec GmbH (https://www.lumitec.solutions)
-# All Right Reserved
-#
-# See LICENSE file for full licensing details.
-##############################################################################
-from . import main
