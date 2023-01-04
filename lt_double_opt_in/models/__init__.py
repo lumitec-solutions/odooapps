8c875809f203587c813d498ae7ce807f4384908b
mim-lumitec <martin.michelic@lumitec.solutions>
2023-01-04 17:11:37 +0100

    Removed app lt_double_opt_in. This app is available in a separate github repository

--- lt_double_opt_in/models/__init__.py
+++
@@ -1,9 +0,0 @@
-##############################################################################
-# Copyright (c) 2022 lumitec GmbH (https://www.lumitec.solutions)
-# All Right Reserved
-#
-# See LICENSE file for full licensing details.
-##############################################################################
-from . import crm_lead
-from . import crm_lead_tag
-from . import crm_lead_token
